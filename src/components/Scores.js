9f600c3faa4c48176a56c56d1bacfa8aa93b9604
vishnusj <vishnusj12@gmail.com>
2022-03-21 22:11:06 +0530

    add styling to cards

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,8 +1,8 @@
 export default function Scores({ score, bestScore }) {
   return (
     <div className="scoreBoard">
-      <div className="score">Score {score}</div>
-      <div className="bestScore">Best Score {bestScore}</div>
+      <div className="score">Score: <span>{score}</span></div>
+      <div className="bestScore">Best Score: <span>{bestScore}</span></div>
     </div>
   );
 }
